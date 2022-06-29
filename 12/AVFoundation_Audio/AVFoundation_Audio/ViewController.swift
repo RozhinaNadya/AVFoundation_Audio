3d80c487014531f63f1ea9f20f4c4256e594f20b
RozhinaNadya <zhuk.09@mail.ru>
2022-06-29 17:36:47 +0200

    Added new songs and audio name label

--- 12/AVFoundation_Audio/AVFoundation_Audio/ViewController.swift
+++ 12/AVFoundation_Audio/AVFoundation_Audio/ViewController.swift
@@ -11,13 +11,16 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var Player = AVAudioPlayer()
+    
+    var playList = ["Queen", "Artbat - keep calm (feat.dino lenny)", "Shah - i don't wanna know (feat.brenda mullen)", "Bad boys blue - you're a woman", "Modern talking - you're my heart youre my soul", "DMX - party up"]
+    
+    lazy var currentSong = playList[0]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+        audioNameLabel.text = currentSong
         do {
-            Player = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Queen", ofType: "mp3")!))
+            Player = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: currentSong, ofType: "mp3")!))
             Player.prepareToPlay()
         }
         catch {
@@ -26,6 +29,8 @@ class ViewController: UIViewController {
         
     }
 
+    @IBOutlet weak var audioNameLabel: UILabel!
+    
     @IBAction func PlayButton(_ sender: Any) {
         Player.play()
     }
@@ -48,4 +53,5 @@ class ViewController: UIViewController {
             print("Already stopped!")
         }
     }
+
 }
